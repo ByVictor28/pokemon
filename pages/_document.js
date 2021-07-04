32f1115d81f06b6129693665daa2451f7cc703a7
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-07-04 18:16:39 -0500

    Icon and Title

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,9 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet"/>
+            <title>Pokemon ByVictor28</title>
+            <link rel="icon" href="/VDS28_2.ico" />
+            <meta property="og:title" content="My page title" key="title" />
         </Head>
         <body>
           <Main />
