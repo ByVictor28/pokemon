68a2630e4283837e7296ee9d197c6506161e6e0a
Victor Manuel Delfin Santos <victormanu31611@gmail.com>
2021-07-04 20:04:49 -0500

    netlify

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet"/>
             <title>Pokemon ByVictor28</title>
             <link rel="icon" href="/VDS28_2.ico" />
